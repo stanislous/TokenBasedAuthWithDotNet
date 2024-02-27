507877ab8b247e9cba970f22cac0a91d98c9e28b
Shehan Vanderputt <shehan.v@dubliw.com>
2024-02-27 10:05:42 +0530

    Basic controller for testing.

---
+++ TokenBasedAuthWithDotNet/Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenBasedAuthWithDotNet.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class NameController : ControllerBase
+{
+    [HttpGet("GetNames")]
+    public IActionResult GetNames()
+    {
+        return Ok(new List<string> { "Adam", "Robert" });
+    }
+}
---
+++ TokenBasedAuthWithDotNet/Models/UserCredential.cs
@@ -0,0 +1,7 @@
+﻿namespace TokenBasedAuthWithDotNet.Models;
+
+public class UserCredential
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
