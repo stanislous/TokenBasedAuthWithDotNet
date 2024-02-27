d695cc338289b62e428504bc3e366f37f03443d1
Shehan Vanderputt <shehan.v@dubliw.com>
2024-02-27 10:08:01 +0530

    Added Token service to iServiceCollection.

--- TokenBasedAuthWithDotNet/Program.cs
+++ TokenBasedAuthWithDotNet/Program.cs
@@ -1,8 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TokenBasedAuthWithDotNet;
 
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwtOption =>
+{
+    var key = config.GetValue<string>("JwtConfig:Key");
+    var keyBytes = Encoding.ASCII.GetBytes(key);
+    jwtOption.SaveToken = true;
+    jwtOption.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateLifetime = true,
+        ValidateAudience = false,
+        ValidateIssuer = false
+    };
+});
+
+builder.Services.AddSingleton<IJwtTokenManager, JwtTokenManager>();
 
 var app = builder.Build();
 
@@ -10,6 +35,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
