eff7c8a9d8fca3df4dd78c548e6a516e0d367067
Shehan Vanderputt <shehan.v@dubliw.com>
2024-02-27 09:57:56 +0530

    Added sample data. This will work as the database.

---
+++ TokenBasedAuthWithDotNet/Data.cs
@@ -0,0 +1,10 @@
+﻿namespace TokenBasedAuthWithDotNet;
+
+public static class Data
+{
+    public static Dictionary<string, string> Users = new Dictionary<string, string>()
+    {
+        {"Shehan", "qazwsx"},
+        {"Vanderputt", "qazwsx"},
+    };
+}
